8d94f806518f054c698e79aa524392ff8db53009
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-09 11:46:50 -0700

    add series of console logs for debugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 // business logic
 
 var count = function(number) {
-  // if ((number % 3 === "ping!") && (number % 5 !== "pong!") || ((number % 15 === "PING-PONG!"))
+console.log(number);
   var numberArr = [];
   $("input:[#number]").each(function() {
   var firstNumber = $(this).val();
   numberArr.push(number);
 })
   for (var index = 0; index < numberArr.length; index += 1) {
-
-
+    alert(index);
+console.log(index);
+return result;
   if (numberArr[index] % 3) {
     $(".numbers").text("ping!");
   } else if (numberArr[index] % 5) {
@@ -17,9 +18,9 @@ var count = function(number) {
   } else if (numberArr[index] % 15) {
     $(".numbers").text("PING-PONG!");
   }
-  // return result;
-  };
 
+  };
+ return result;
 
 
 // user interface logic
@@ -27,12 +28,14 @@ $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     event.preventDefault();
 
-    var number = parseInt( ($("input#number").val().split("")));
+    var number = parseInt ($("input#number").val());
     var result = count(number);
-    if ("#number" === 0) {
-      $("#result").text("Try again");
-    }
+console.log(inputnumber);
 
+    // if ("#number" === 0) {
+    //   $("#result").text("Try again");
+    // }
+console.log(number);
     $(".numbers").text(count);
     $("#result").show();
   });
