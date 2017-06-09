7f9af47af635990e7f9870041ef86e77d2ebb514
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-09 16:48:44 -0700

    wrap up for push

--- js/scripts.js
+++ js/scripts.js
@@ -3,29 +3,30 @@
 var count = function(number) {
   var counting = [];
   counting.push(count);
-  var result = [];
+  // var result = [];
   for (var index = 0; index < counting.length; index += 1){
-     if (counting[index] % 3) {
-       $(".numbers").text("ping!");
-     } else if (counting[index] % 5) {
-       $(".numbers").text("pong!");
-     } else if (counting[index] % 15) {
-       $(".numbers").text("PING-PONG!");
-      result.push(index);
+    //  if (counting[index] % 3) {
+    //    $(".numbers").text("ping!");
+    //  } else if (counting[index] % 5) {
+    //    $(".numbers").text("pong!");
+    //  } else if (counting[index] % 15) {
+    //    $(".numbers").text("PING-PONG!");
+    //   result.push(index);
 console.log(result);
      }
-     return result;
+    //  return result;
 };
-console.log(number);
-};
+console.log();
+// };
 
 // user interface logic
 $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     event.preventDefault();
     var number = parseInt ($("input#number").val());
+    var total = count(number);
 console.log(number);
-    $("#result").append("<li>" + "</li>");
+    $("ul").append("<li>" + "</li>");
 console.log(number);
   });
 });
