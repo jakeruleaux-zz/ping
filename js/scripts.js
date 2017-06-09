c0cad765ced64d527b48d84772e74c6fc9328b76
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-09 14:59:02 -0700

    and background image and text color

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 // business logic
 
 var count = function(number) {
+  var counting = [];
+  counting.push(count);
+
+  for (var index = 0; index < counting.length; index += 1){
+     if (counting[index] % 3) {
+       $(".numbers").text("ping!");
+     } else if (counting[index] % 5) {
+       $(".numbers").text("pong!");
+     } else if (counting[index] % 15) {
+       $(".numbers").text("PING-PONG!");
+     }
+
+ // return result;
+  };
+
+
+
 console.log(number);
-  
+
 
 //   var numberArr = [];
 //   $("input:[#number]").each(function() {
@@ -22,7 +39,7 @@ console.log(number);
 //   }
 
   };
- // return result;
+
 
 
 // user interface logic
@@ -37,8 +54,13 @@ console.log(number);
     // if ("#number" === 0) {
     //   $("#result").text("Try again");
     // }
+$("#result").append("<li>" + "/<li>");
+
+
+
+
+
 console.log(number);
-    $(".numbers").text(number);
-    $("#result").show();
+
+    //  $("#result").show();
   });
-});
