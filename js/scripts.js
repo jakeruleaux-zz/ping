d91bfd35bfcca5064a05695a5d14d2049bf6cf9f
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-09 11:27:32 -0700

    clean up indentation and spacing in js

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@
 
 var count = function(number) {
   // if ((number % 3 === "ping!") && (number % 5 !== "pong!") || ((number % 15 === "PING-PONG!"))
-var numberArr = [];
-$("input:[#number]").each(function() {
+  var numberArr = [];
+  $("input:[#number]").each(function() {
   var firstNumber = $(this).val();
   numberArr.push(number);
 })
-for (var index = 0; index < numberArr.length; index += 1) {
+  for (var index = 0; index < numberArr.length; index += 1) {
 
 
   if (numberArr[index] % 3) {
@@ -27,18 +27,14 @@ $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     event.preventDefault();
 
-
-
     var number = parseInt( ($("input#number").val().split("")));
     var result = count(number);
     if ("#number" === 0) {
       $("#result").text("Try again");
     }
+
     $(".numbers").text(count);
-
-             // same as writing if (result === false)
-
-
     $("#result").show();
   });
 });
+};
