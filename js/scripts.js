ba7102657c5011a2ee154a9a22cc910b0f9ba1b6
jacob ruleaux <jakeruleaux>
2017-06-16 14:47:33 -0700

    updates js

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,22 @@
 
 var count = function(number) {
   var counting = [];
-  counting.push(count);
-  // var result = [];
-  for (var index = 0; index < counting.length; index += 1){
-    //  if (counting[index] % 3) {
-    //    $(".numbers").text("ping!");
-    //  } else if (counting[index] % 5) {
-    //    $(".numbers").text("pong!");
-    //  } else if (counting[index] % 15) {
-    //    $(".numbers").text("PING-PONG!");
-    //   result.push(index);
-console.log(result);
-     }
 
+  for (var i = 0; i < counting.length; i += 1){
+      if (counting[i] % 3) {
+        return counting;
+      } else (counting[i] % 5) {
+        return counting;
+      }
 };
 console.log();
-
-
-// user interface logic
 $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     event.preventDefault();
-    var number = parseInt ($("input#number").val());
+    var number = parseInt($("input#number").val());
     var total = count(number);
-console.log(number);
-    $("ul").append("<li>" + "</li>");
-console.log(number);
+    total.forEach(function() {
+    $("#result").append("<li>" + "</li>");
+  });
   });
 });
